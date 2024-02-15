803ba415ba0d5bda1f4fe97dca54b18cca1b89d4
swapno963 <swapno963@gmail.com>
2024-02-15 18:35:04 +0600

    some style chandeg

--- src/Components/ClassShedule.jsx
+++ src/Components/ClassShedule.jsx
@@ -26,33 +26,33 @@ export default function classNameShedule() {
     // Add more data as needed
   ];
   return (
-    <div className="pt-12 mt-12 ">
-      <h3 className="text-center text-5xl text-blue-700">CLASSES SCHEDULE</h3>
-      <p className="text-center pt-5">
+    <div className="pt-12 mt-12 bg-blue-50 py-12">
+      <h3 className="text-center text-5xl font-bold text-blue-700">CLASSES SCHEDULE</h3>
+      <p className="text-center pt-5 font-semibold leading-relaxed text-md">
         Lorem ipsum dolor sit amet consectetur adipisicing
-        <br />
-        elit. Consequatur, possimus nemo animi id ea voluptatibus sunt amet unde
-        explicabo dolorum, earum quos nam similique <br /> aliquam
+     
+        elit. Consequatur, possimus nemo    <br />animi id ea voluptatibus sunt amet unde
+        explicabo dolorum,  earum quos nam <br /> similiquealiquam
         reprehenderit, dolor non ipsum quo.
       </p>
-      <p className="text-center text-3xl py-12">Sunday / Monday / Tusday</p>
+      <p className="text-center text-3xl py-12 font-bold text-gray-600">Sunday / Monday / Tusday</p>
       <div className="flex items-center  justify-center  container mx-auto">
         <table className="min-w-full bg-white border border-gray-300 ">
           <thead>
             <tr className="bg-gray-100 border-b">
-              <th className="py-2 px-4 border-r">Fitness Class</th>
-              <th className="py-2 px-4 border-r">Morning</th>
-              <th className="py-2 px-4 border-r">Afternoon</th>
-              <th className="py-2 px-4">Evening</th>
+              <th className="py-2 px-4 border-r text-xl text-gray-600 leading-relaxed">Fitness Class</th>
+              <th className="py-2 px-4 border-r  text-xl text-gray-600 leading-relaxed">Morning</th>
+              <th className="py-2 px-4 border-r  text-xl text-gray-600 leading-relaxed">Afternoon</th>
+              <th className="py-2 px-4  text-xl text-gray-600 leading-relaxed">Evening</th>
             </tr>
           </thead>
           <tbody>
             {data.map((row) => (
               <tr key={row.id} className="border-b ">
-                <td className="py-12 text-center border-r">{row.type}</td>
-                <td className="py-12 text-center border-r">{row.time1}</td>
-                <td className="py-12 text-center border-r">{row.time2}</td>
-                <td className="py-12 text-center">{row.time3}</td>
+                <td className="py-12 text-center border-r font-medium text-gray-700 leading-relaxed text-3xl">{row.type}</td>
+                <td className="py-12 text-center border-r  text-gray-700 leading-relaxed text-3xl">{row.time1}</td>
+                <td className="py-12 text-center border-r  text-gray-700 leading-relaxed text-3xl">{row.time2}</td>
+                <td className="py-12 text-center  text-gray-700 leading-relaxed text-3xl">{row.time3}</td>
               </tr>
             ))}
           </tbody>
--- src/Components/Trainer.jsx
+++ src/Components/Trainer.jsx
@@ -55,11 +55,11 @@ const posts = [
   
   export default function Trainer() {
     return (
-      <div className="bg-white py-24 sm:py-32">
+      <div className="bg-white py-24 sm:py-32 font-montserrat">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">We provide the best trainer</h2>
-            <p className="mt-2 text-lg leading-8 text-gray-600">
+            <p className="mt-4 text-lg leading-8 text-gray-600">
               Learn how to grow your business with our expert advice.
             </p>
           </div>
